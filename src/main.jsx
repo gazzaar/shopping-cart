9d5db8d6c94857c0770fba5c526d6de12bbc116b
gazzaar <gazzarDev@gmail.com>
2024-10-28 13:44:07 +0300

    Implement initial router

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,19 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './App.jsx';
+import { ErrorPage } from './components/error/ErrorPage.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <ErrorPage />,
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}></RouterProvider>
   </StrictMode>,
 );
